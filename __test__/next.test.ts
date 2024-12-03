399f9203a27db9fb9fdfee7982848347f71aa0c4
concefly <concefly@foxmail.com>
2024-12-04 00:03:33 +0800

    refactor defineComponent width rxjs

--- __test__/next.test.ts
+++ __test__/next.test.ts
@@ -1,5 +1,6 @@
 import { it, expect } from 'vitest';
 import { blueprint, defineComponent, defineContext } from '../src/next';
+import { combineLatest } from 'rxjs';
 
 it('Single Component Lifecycle', () => {
   const timelines: string[] = [];
@@ -9,20 +10,13 @@ it('Single Component Lifecycle', () => {
     setup: ctx => {
       timelines.push(`a init`);
 
-      ctx.onBeforeUpdate = (props, changes) => {
-        timelines.push(`a before: ${JSON.stringify(props)}, changes: ${JSON.stringify([...changes])}`);
-      };
-
-      ctx.onUpdate = (props, changes) => {
-        timelines.push(`a update: ${JSON.stringify(props)}, changes: ${JSON.stringify([...changes])}`);
-      };
-
-      ctx.onAfterUpdate = (props, changes) => {
-        timelines.push(`a after: ${JSON.stringify(props)}, changes: ${JSON.stringify([...changes])}`);
-      };
+      const sub = combineLatest([ctx.P.n$]).subscribe(([n]) => {
+        timelines.push(`a update: n=${n}`);
+      });
 
       return () => {
         timelines.push('a dispose');
+        sub.unsubscribe();
       };
     },
   });
@@ -40,15 +34,9 @@ it('Single Component Lifecycle', () => {
   expect(timelines).toMatchInlineSnapshot(`
     [
       "a init",
-      "a before: {"n":1}, changes: [["n",null]]",
-      "a update: {"n":1}, changes: [["n",null]]",
-      "a after: {"n":1}, changes: [["n",null]]",
-      "a before: {"n":2}, changes: [["n",1]]",
-      "a update: {"n":2}, changes: [["n",1]]",
-      "a after: {"n":2}, changes: [["n",1]]",
-      "a before: {"n":3}, changes: [["n",2]]",
-      "a update: {"n":3}, changes: [["n",2]]",
-      "a after: {"n":3}, changes: [["n",2]]",
+      "a update: n=1",
+      "a update: n=2",
+      "a update: n=3",
       "a dispose",
     ]
   `);
@@ -62,20 +50,13 @@ it('Single Component Lifecycle with initProps', () => {
     setup: ctx => {
       timelines.push(`a init`);
 
-      ctx.onBeforeUpdate = (props, changes) => {
-        timelines.push(`a before: ${JSON.stringify(props)}, changes: ${JSON.stringify([...changes])}`);
-      };
-
-      ctx.onUpdate = (props, changes) => {
-        timelines.push(`a update: ${JSON.stringify(props)}, changes: ${JSON.stringify([...changes])}`);
-      };
-
-      ctx.onAfterUpdate = (props, changes) => {
-        timelines.push(`a after: ${JSON.stringify(props)}, changes: ${JSON.stringify([...changes])}`);
-      };
+      const sub = combineLatest([ctx.P.n$]).subscribe(([n]) => {
+        timelines.push(`a update: n=${n}`);
+      });
 
       return () => {
         timelines.push('a dispose');
+        sub.unsubscribe();
       };
     },
   });
@@ -86,9 +67,7 @@ it('Single Component Lifecycle with initProps', () => {
   expect(timelines).toMatchInlineSnapshot(`
     [
       "a init",
-      "a before: {"n":2}, changes: [["n",null]]",
-      "a update: {"n":2}, changes: [["n",null]]",
-      "a after: {"n":2}, changes: [["n",null]]",
+      "a update: n=2",
       "a dispose",
     ]
   `);
@@ -132,20 +111,13 @@ it('Hierarchy Components Lifecycle', () => {
     setup: ctx => {
       timelines.push(`b<${ctx.key}> init`);
 
-      ctx.onBeforeUpdate = (props, changes) => {
-        timelines.push(`b<${ctx.key}> before: ${JSON.stringify(props)}, changes: ${JSON.stringify([...changes])}`);
-      };
-
-      ctx.onUpdate = (props, changes) => {
-        timelines.push(`b<${ctx.key}> update: ${JSON.stringify(props)}, changes: ${JSON.stringify([...changes])}`);
-      };
-
-      ctx.onAfterUpdate = (props, changes) => {
-        timelines.push(`b<${ctx.key}> after: ${JSON.stringify(props)}, changes: ${JSON.stringify([...changes])}`);
-      };
+      const sub = combineLatest([ctx.P.b$]).subscribe(([n]) => {
+        timelines.push(`b<${ctx.key}> update: b=${n}`);
+      });
 
       return () => {
         timelines.push(`b<${ctx.key}> dispose`);
+        sub.unsubscribe();
       };
     },
   });
@@ -155,24 +127,17 @@ it('Hierarchy Components Lifecycle', () => {
     setup: ctx => {
       timelines.push(`a<${ctx.key}> init`);
 
-      ctx.onBeforeUpdate = (props, changes) => {
-        timelines.push(`a<${ctx.key}> before: ${JSON.stringify(props)}, changes: ${JSON.stringify([...changes])}`);
-      };
+      const sub = combineLatest([ctx.P.a$]).subscribe(([a]) => {
+        timelines.push(`a<${ctx.key}> update: a=${a}`);
 
-      ctx.onUpdate = (props, changes) => {
-        timelines.push(`a<${ctx.key}> update: ${JSON.stringify(props)}, changes: ${JSON.stringify([...changes])}`);
-
-        if (props.a === 1) return [blueprint(B, { b: 1 }, '1')];
-        if (props.a === 2) return [blueprint(B, { b: 1.1 }, '1'), blueprint(B, { b: 2 }, '2')];
-        if (props.a === 3) return [blueprint(B, { b: 2.1 }, '2')];
-      };
-
-      ctx.onAfterUpdate = (props, changes) => {
-        timelines.push(`a<${ctx.key}> after: ${JSON.stringify(props)}, changes: ${JSON.stringify([...changes])}`);
-      };
+        if (a === 1) ctx.blueprints$.next([blueprint(B, { b: 1 }, '1')]);
+        if (a === 2) ctx.blueprints$.next([blueprint(B, { b: 1.1 }, '1'), blueprint(B, { b: 2 }, '2')]);
+        if (a === 3) ctx.blueprints$.next([blueprint(B, { b: 2.1 }, '2')]);
+      });
 
       return () => {
         timelines.push(`a<${ctx.key}> dispose`);
+        sub.unsubscribe();
       };
     },
   });
@@ -181,40 +146,22 @@ it('Hierarchy Components Lifecycle', () => {
 
   root.update({ a: 2 });
   root.update({ a: 3 });
-  root.update({ a: 4 });
 
   root.dispose();
 
   expect(timelines).toMatchInlineSnapshot(`
     [
       "a<root> init",
-      "a<root> before: {"a":1}, changes: [["a",null]]",
-      "a<root> update: {"a":1}, changes: [["a",null]]",
-      "a<root> after: {"a":1}, changes: [["a",null]]",
+      "a<root> update: a=1",
       "b<1> init",
-      "b<1> before: {"b":1}, changes: [["b",null]]",
-      "b<1> update: {"b":1}, changes: [["b",null]]",
-      "b<1> after: {"b":1}, changes: [["b",null]]",
-      "a<root> before: {"a":2}, changes: [["a",1]]",
-      "a<root> update: {"a":2}, changes: [["a",1]]",
-      "a<root> after: {"a":2}, changes: [["a",1]]",
-      "b<1> before: {"b":1.1}, changes: [["b",1]]",
-      "b<1> update: {"b":1.1}, changes: [["b",1]]",
-      "b<1> after: {"b":1.1}, changes: [["b",1]]",
+      "b<1> update: b=1",
+      "a<root> update: a=2",
+      "b<1> update: b=1.1",
       "b<2> init",
-      "b<2> before: {"b":2}, changes: [["b",null]]",
-      "b<2> update: {"b":2}, changes: [["b",null]]",
-      "b<2> after: {"b":2}, changes: [["b",null]]",
-      "a<root> before: {"a":3}, changes: [["a",2]]",
-      "a<root> update: {"a":3}, changes: [["a",2]]",
-      "a<root> after: {"a":3}, changes: [["a",2]]",
+      "b<2> update: b=2",
+      "a<root> update: a=3",
       "b<1> dispose",
-      "b<2> before: {"b":2.1}, changes: [["b",2]]",
-      "b<2> update: {"b":2.1}, changes: [["b",2]]",
-      "b<2> after: {"b":2.1}, changes: [["b",2]]",
-      "a<root> before: {"a":4}, changes: [["a",3]]",
-      "a<root> update: {"a":4}, changes: [["a",3]]",
-      "a<root> after: {"a":4}, changes: [["a",3]]",
+      "b<2> update: b=2.1",
       "b<2> dispose",
       "a<root> dispose",
     ]
@@ -234,11 +181,13 @@ it('Hierarchy Components Context', () => {
       const userContextVal = ctx.getContext(userContext);
       timelines.push(`b<${ctx.key}> user: ${userContextVal.value}`);
 
-      ctx.onUpdate = () => {
+      const sub = combineLatest([ctx.P.b$]).subscribe(() => {
         timelines.push(`b<${ctx.key}> update user: ${userContextVal.value}`);
-      };
+      });
 
-      return () => {};
+      return () => {
+        sub.unsubscribe();
+      };
     },
   });
 
@@ -252,15 +201,17 @@ it('Hierarchy Components Context', () => {
 
       userContextVal.value = 'user_1';
 
-      ctx.onUpdate = props => {
-        if (props.a === 2) {
+      const sub = combineLatest([ctx.P.a$]).subscribe(([a]) => {
+        if (a === 2) {
           userContextVal.value = 'user_2';
         }
 
-        return [blueprint(B, { b: props.a }, '1')];
-      };
+        ctx.blueprints$.next([blueprint(B, { b: a }, '1')]);
+      });
 
-      return () => {};
+      return () => {
+        sub.unsubscribe();
+      };
     },
   });
 
--- src/next.ts
+++ src/next.ts
@@ -1,3 +1,5 @@
+import { map, scan, Subject } from 'rxjs';
+
 export type IOptions<_K, V> = {
   isEqual: (a?: V, b?: V) => boolean;
   getVersion: (v: V) => string | number;
@@ -35,14 +37,13 @@ export type IOnDisposeCB = () => void;
 export type ICreateContextCB = <K extends string, T>(key: IContextDefinition<K, T>) => { value: T };
 export type IGetContextCB = <K extends string, T>(key: IContextDefinition<K, T>) => { value: T };
 
+export type IPropSubjects<P extends Record<string, any>> = { [K in keyof P as K extends string ? `${K}$` : never]: Subject<P[K]> };
+
 export type ISetupCallback<P extends Record<string, any>> = (ctx: {
   key: string;
 
-  initProps: P;
-
-  onBeforeUpdate: IOnBeforeUpdateCB<P>;
-  onUpdate: IOnUpdateCB<P>;
-  onAfterUpdate: IOnAfterUpdateCB<P>;
+  P: IPropSubjects<P>;
+  blueprints$: Subject<Blueprint[]>;
 
   createContext: ICreateContextCB;
   getContext: IGetContextCB;
@@ -92,16 +93,16 @@ export function defineComponent<P extends Record<string, any>>(def: IComponentDe
     const contextStore = new Map<string, any>();
     const instance: IComponentInstance<P> = { key, contextStore, parent, createContext, getContext, update, dispose };
 
-    const ctx: Parameters<ISetupCallback<P>>[0] = {
-      key,
-      initProps,
-      onBeforeUpdate: () => {},
-      onUpdate: () => {},
-      onAfterUpdate: () => {},
+    const propSubjects: IPropSubjects<P> = {} as any;
 
-      createContext,
-      getContext,
-    };
+    const inputProps$ = new Subject<P>();
+    const blueprints$ = new Subject<Blueprint[]>();
+
+    for (const k in initProps) {
+      propSubjects[`${k}$`] = new Subject<any>() as any;
+    }
+
+    const ctx: Parameters<ISetupCallback<P>>[0] = { key, P: propSubjects, blueprints$, createContext, getContext };
 
     // setup
     const onDisposeCB = def.setup(ctx);
@@ -109,29 +110,152 @@ export function defineComponent<P extends Record<string, any>>(def: IComponentDe
     let disposed = false;
     let children: _IChild[] = [];
 
-    const commonKeys = new Set<string>();
-    const changes = new Map<string, any>();
+    const inputSub = inputProps$
+      .pipe(
+        scan(
+          (lastInfos, cur) => {
+            const newInfos: Record<string, { value: any; version: any; changed: boolean }> = {};
 
-    let oldProps: any = {};
-    let oldVersions: any = {};
+            for (const [key, newVal] of Object.entries(cur)) {
+              const getVersion = def.options?.[key]?.getVersion ?? DefaultOptions.getVersion;
+              const isEqual = def.options?.[key]?.isEqual ?? DefaultOptions.isEqual;
+              const useVersionCheck = def.options?.[key]?.useVersionCheck ?? DefaultOptions.useVersionCheck;
 
-    const oldChildMap = new Map<string, _IChild>();
-    const newChildMap = new Map<string, _IChild>();
+              if (!lastInfos[key]) {
+                newInfos[key] = { value: newVal, version: useVersionCheck ? getVersion(newVal) : undefined, changed: true };
+                continue;
+              }
 
-    const _toDisposeKeys = new Set<string>();
-    const _toCreateKeys = new Set<string>();
-    const _toUpdateKeys = new Set<string>();
+              let toCompareA: any;
+              let toCompareB: any;
+              let newVersion: any = undefined;
 
-    const _disposeChild = (child: _IChild) => {
+              if (useVersionCheck) {
+                toCompareA = lastInfos[key].version;
+                newVersion = getVersion(newVal);
+                toCompareB = newVersion;
+              } else {
+                toCompareA = lastInfos[key].value;
+                toCompareB = newVal;
+              }
+
+              newInfos[key] = { value: newVal, version: newVersion, changed: !isEqual(toCompareA, toCompareB) };
+            }
+
+            return newInfos;
+          },
+          {} as Record<string, { value: any; version: any; changed: boolean }>
+        ),
+        map(infos => {
+          const entries = Object.entries(infos).filter(([_, info]) => info.changed);
+          // const changes = new Set<string>(entries.map(([key]) => key));
+
+          for (const [k, v] of entries) {
+            propSubjects[`${k}$`].next(v.value);
+          }
+        })
+      )
+      .subscribe();
+
+    const updateSub = blueprints$
+      .pipe(
+        scan(
+          (lastInfo, nextChildren) => {
+            if (disposed) throw new Error('Already disposed');
+
+            const { children: lastChildren, oldChildMap, newChildMap, commonKeys, _toDisposeKeys, _toCreateKeys, _toUpdateKeys } = lastInfo;
+
+            oldChildMap.clear();
+            newChildMap.clear();
+            commonKeys.clear();
+
+            _toDisposeKeys.clear();
+            _toCreateKeys.clear();
+            _toUpdateKeys.clear();
+
+            for (const c of lastChildren) oldChildMap.set(c.key, c);
+            for (const c of nextChildren) newChildMap.set(c.key, c);
+
+            for (const c of lastChildren) commonKeys.add(c.key);
+            for (const c of nextChildren) commonKeys.add(c.key);
+
+            for (let i = 0; i < lastChildren.length; i++) {
+              const _n = lastChildren[i];
+              _toUpdateKeys.add(_n.key);
+              if (!newChildMap.has(_n.key)) _toDisposeKeys.add(_n.key);
+            }
+
+            for (let i = 0; i < nextChildren.length; i++) {
+              const _n = nextChildren[i];
+              _toUpdateKeys.add(_n.key);
+              if (!oldChildMap.has(_n.key)) _toCreateKeys.add(_n.key);
+            }
+
+            // 卸载
+            for (const _k of _toDisposeKeys) {
+              _toUpdateKeys.delete(_k);
+
+              const child = oldChildMap.get(_k)!;
+
+              _disposeChild(child);
+            }
+
+            // 按顺序创建 or 更新
+            for (const child of nextChildren) {
+              // create
+              if (_toCreateKeys.has(child.key)) {
+                _createChild(child);
+              }
+
+              // update
+              else if (_toUpdateKeys.has(child.key)) {
+                const prev = oldChildMap.get(child.key)!;
+                const next = newChildMap.get(child.key)!;
+
+                // 类型相同，递归更新
+                if (prev.factory === next.factory) {
+                  if (!prev.ins) throw new Error('prev.ins not exists');
+                  if (!next.ins) next.ins = prev.ins; // 传递实例
+
+                  // set props
+                  next.ins.update(next.props);
+                }
+
+                // 类型不同，卸载 prev，创建 next
+                else {
+                  _disposeChild(prev);
+                  _createChild(next);
+                }
+              }
+            }
+
+            children = nextChildren;
+            lastInfo.children = nextChildren;
+            return lastInfo;
+          },
+          {
+            children: [] as _IChild[],
+            oldChildMap: new Map<string, _IChild>(),
+            newChildMap: new Map<string, _IChild>(),
+            commonKeys: new Set<string>(),
+            _toDisposeKeys: new Set<string>(),
+            _toCreateKeys: new Set<string>(),
+            _toUpdateKeys: new Set<string>(),
+          }
+        )
+      )
+      .subscribe();
+
+    function _disposeChild(child: _IChild) {
       if (child.ins) {
         child.ins.dispose();
         child.ins = undefined;
       }
-    };
+    }
 
-    const _createChild = (child: _IChild) => {
+    function _createChild(child: _IChild) {
       child.ins = child.factory.create(child.key, child.props, instance);
-    };
+    }
 
     function createContext(key: IContextDefinition<string, any>) {
       if (contextStore.has(key.key)) throw new Error('key already exists');
@@ -158,118 +282,7 @@ export function defineComponent<P extends Record<string, any>>(def: IComponentDe
 
     function update(newProps: P) {
       if (disposed) throw new Error('Already disposed');
-
-      // calc changes
-      commonKeys.clear();
-      changes.clear();
-
-      for (const k of Object.keys(oldProps)) commonKeys.add(k);
-      for (const k of Object.keys(newProps)) commonKeys.add(k);
-
-      let newVersions: any = {};
-
-      for (const k of commonKeys) {
-        const getVersion = def.options?.[k]?.getVersion ?? DefaultOptions.getVersion;
-        const isEqual = def.options?.[k]?.isEqual ?? DefaultOptions.isEqual;
-        const useVersionCheck = def.options?.[k]?.useVersionCheck ?? DefaultOptions.useVersionCheck;
-
-        const oldVal = oldProps[k];
-        const newVal = newProps[k];
-
-        let toCompareA: any;
-        let toCompareB: any;
-
-        if (useVersionCheck) {
-          toCompareA = oldVersions[k];
-
-          newVersions[k] = getVersion(newVal);
-          toCompareB = newVersions[k];
-        } else {
-          toCompareA = oldVal;
-          toCompareB = newVal;
-        }
-
-        if (!isEqual(toCompareA, toCompareB)) {
-          changes.set(k, oldVal);
-        }
-      }
-
-      // 没有变化，不处理
-      if (changes.size === 0) return;
-
-      oldChildMap.clear();
-      newChildMap.clear();
-
-      ctx.onBeforeUpdate?.(newProps, changes);
-
-      const nextChildren = (ctx.onUpdate?.(newProps, changes) ?? []) as _IChild[];
-      ctx.onAfterUpdate?.(newProps, changes);
-
-      for (const c of children) oldChildMap.set(c.key, c);
-      for (const c of nextChildren) newChildMap.set(c.key, c);
-
-      commonKeys.clear();
-
-      for (const c of children) commonKeys.add(c.key);
-      for (const c of nextChildren) commonKeys.add(c.key);
-
-      _toDisposeKeys.clear();
-      _toCreateKeys.clear();
-      _toUpdateKeys.clear();
-
-      for (let i = 0; i < children.length; i++) {
-        const _n = children[i];
-        _toUpdateKeys.add(_n.key);
-        if (!newChildMap.has(_n.key)) _toDisposeKeys.add(_n.key);
-      }
-
-      for (let i = 0; i < nextChildren.length; i++) {
-        const _n = nextChildren[i];
-        _toUpdateKeys.add(_n.key);
-        if (!oldChildMap.has(_n.key)) _toCreateKeys.add(_n.key);
-      }
-
-      // 卸载
-      for (const _k of _toDisposeKeys) {
-        _toUpdateKeys.delete(_k);
-
-        const child = oldChildMap.get(_k)!;
-        _disposeChild(child);
-      }
-
-      // 按顺序创建 or 更新
-      for (const child of nextChildren) {
-        // create
-        if (_toCreateKeys.has(child.key)) {
-          _createChild(child);
-        }
-
-        // update
-        else if (_toUpdateKeys.has(child.key)) {
-          const prev = oldChildMap.get(child.key)!;
-          const next = newChildMap.get(child.key)!;
-
-          // 类型相同，递归更新
-          if (prev.factory === next.factory) {
-            if (!prev.ins) throw new Error('prev.ins not exists');
-            if (!next.ins) next.ins = prev.ins; // 传递实例
-
-            // set props
-            next.ins.update(next.props);
-          }
-
-          // 类型不同，卸载 prev，创建 next
-          else {
-            _disposeChild(prev);
-            _createChild(next);
-          }
-        }
-      }
-
-      oldProps = newProps;
-      oldVersions = newVersions;
-
-      children = nextChildren;
+      inputProps$.next(newProps);
     }
 
     function dispose() {
@@ -280,6 +293,8 @@ export function defineComponent<P extends Record<string, any>>(def: IComponentDe
       }
 
       onDisposeCB();
+      updateSub.unsubscribe();
+      inputSub.unsubscribe();
 
       children.length = 0;
       disposed = true;
